b1699e7589924d5f971ddbd8cf4a8ae12a485449
Ubuntu <ubuntu@ip-172-31-47-40.us-west-2.compute.internal>
2025-08-26 11:29:40 +0000

    Added mai

--- deploy_django_app.sh
+++ deploy_django_app.sh
@@ -14,11 +14,12 @@ clone_rep(){
 install_dependencies(){
     echo "Installing dependencies"
     sudo apt-get update
-    sudo apt-get install docker.io nginx docker-compose -y
+    sudo apt-get install docker.io nginx docker-compose sendmail -y
     return 0
 }
 update_system(){
-	sudo systemctl enable nginx && sudo systemctl enable docker || return 1
+	sudo chown $USER /var/run/docker.sock || return 1 
+sudo systemctl stop nginx && sudo systemctl disable nginx && sudo systemctl enable nginx && sudo systemctl enable docker || return 1
     sudo usermod -aG docker $USER || return 1 
     return 0
 }
@@ -29,7 +30,13 @@ run_container(){
 	docker-compose up -d || return 1 
     return 0
 }
-
+sendmail_body()
+{
+    to="saniyabagwan1111@gmail.com"
+    sub="Deployment Status"
+    message="Deployment Done successfully"
+    echo -e "Subject:/$sub\n\n$messgae"| sendmail $to
+}
 echo "*********** Deployment Started ************"
 
 if ! clone_rep; then 
@@ -51,5 +58,7 @@ if ! run_container; then
     echo "Container failed"
     exit 1
 fi
-
+if ! sendmail_body ; then
+	echo "Mail not send "
+fi
 echo "*********** Deployment Done ************"
